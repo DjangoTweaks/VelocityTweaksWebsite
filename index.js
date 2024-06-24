a21b35b48a1dd7090870b86cb29850f800509207
Aadity Dawkar <dawkarad2002@gmail.com>
2024-06-24 19:56:12 +0530

    reviews code added on backend

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 
 
 const queryRoutes = require("./routes/queryRoutes");
-
+const reviewRoutes = require("./routes/reviewRoutes")
 
 
 app.use(cors());
@@ -14,6 +14,7 @@ app.use(express.urlencoded({extended:false}))
 
 
 app.use('/api/queries', queryRoutes);
+app.use('/api/review', reviewRoutes);
 
 const{connectMongoDB}= require('./connect')
 connectMongoDB('mongodb://127.0.0.1:27017/d1')
