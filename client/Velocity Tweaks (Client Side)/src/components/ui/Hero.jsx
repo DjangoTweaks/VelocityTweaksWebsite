3f8fd1b6503e13a602f64b9c78f833a36234dfff
DjangoTweaks <djangotweaks@gmail.com>
2024-06-29 03:41:31 +0530

    added review complete carousel

--- client/Velocity Tweaks (Client Side)/src/components/ui/Hero.jsx
+++ client/Velocity Tweaks (Client Side)/src/components/ui/Hero.jsx
@@ -1,34 +1,48 @@
 import React from "react";
-import backgroundHeroImage from '../HeroAttempt.png'
+import backgroundHeroImage from "../HeroAttempt.png";
+import { Link } from "react-router-dom";
 export default function Hero() {
   return (
-    <div className="bg-cover bg-center h-screen"  style={{backgroundImage: `url(${backgroundHeroImage})`}}>
-{/* "background-image: url('your-image-url.jpg');" */}
-    {/* <div className="relative h-screen z-10" ></div> */}
+    <div
+      className="bg-cover bg-center h-screen"
+      style={{ backgroundImage: `url(${backgroundHeroImage})` }}
+    >
+      {/* "background-image: url('your-image-url.jpg');" */}
+      {/* <div className="relative h-screen z-10" ></div> */}
 
-    <div className="container  mx-auto md:pt-20 pt-10">
-      
-      <div className="text-[#FFFFFF] font-Inter tracking-tight  	 font-semibold text-8xl">
-        Elevate
-      </div>
+      <div className="container  mx-auto md:pt-20 pt-10">
+        <div className="text-[#FFFFFF] font-Inter tracking-tight  	 font-semibold text-8xl">
+          Elevate
+        </div>
 
-      <div className="text-[#FC2B27] font-Inter tracking-tight 		font-semibold text-8xl">
-        Boost
-      </div>
+        <div className="text-[#FC2B27] font-Inter tracking-tight 		font-semibold text-8xl">
+          Boost
+        </div>
 
-      <div className="text-[#07A4FF] font-Inter tracking-tight	 	font-semibold text-8xl">
-        Conquer.
-      </div>
+        <div className="text-[#07A4FF] font-Inter tracking-tight	 	font-semibold text-8xl">
+          Conquer.
+        </div>
 
-      <div className="text-white block w-[410px] tracking-wider font-light text-xl	leading-9 font-Roboto md:pt-10 pt-16">
-        Unlock the full potential of your gaming rig with Velocity Tweaks, a
-        leading company specializing in a utility designed to optimize your PC
-        for maximum FPS. Trusted by countless professional players and
-        customers, Velocity Tweaks ensures the lowest possible latency, input
-        delay, and ping, making it the best choice in the scene.{" "}
+        <div className="text-white   block w-[670px] tracking-wider font-light text-xl	leading-9 font-Roboto md:pt-16 pt-16">
+          Unlock the full potential of your gaming rig with Velocity Tweaks, a
+          leading company specializing in a utility designed to optimize your PC
+          for maximum FPS. Trusted by countless professional players and
+          customers, Velocity Tweaks ensures the lowest possible latency, input
+          delay, and ping, making it the best choice in the scene.{" "}
+        </div>
+
+        <div className="flex">
+          <Link to="store">
+            <button className="text-white bg-[#FC2B27] transition-all ease-in duration-75 hover:bg-[#AB1D1A]  font-Inter font-medium text-lg rounded-full mt-36 h-16  w-40 p-3">
+              Buy Now
+            </button>
+          </Link>
+
+          <button className="text-white bg-black border border-white border-2 transition-all ease-in duration-75 hover:bg-[#1A1A1A]  text-lg font-Inter font-medium rounded-full ml-4 mt-36 h-16  w-40 p-3">
+            Learn More
+          </button>
+        </div>
       </div>
     </div>
-    </div>
-
   );
 }
--- client/Velocity Tweaks (Client Side)/src/components/ui/Review.jsx
+++ client/Velocity Tweaks (Client Side)/src/components/ui/Review.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import ControlledCarousel from "../ReviewCarousel";
 
-export default function Review()
-{
+export default function Review() {
+  // const settings = {
+  //     dots: true,
+  //     infinite: true,
+  //     speed: 500,
+  //     slidesToShow: 3,
+  //     slidesToScroll: 3
+  //   };
 
-    return <div className="pt-24 bg-gradient-to-b from-[#0a0303] from-20% to-[#000000] top-80% " >
-        <div className="flex justify-center bg-gradient-to-r from-[#0069cc] via-[#9BAAE5] to-[#7b3636] bg-clip-text text-transparent font-Inter font-medium  opacity-70 text-4xl" >But Don't Take Our Word For It</div>
-        <div className="flex justify-center pt-6 text-white font-Inter text-7xl font-semibold" >Customer Testimonials</div>
+  return (
+    <div>
+      <div className="pt-24  bg-gradient-to-b from-[#0a0303] from-20% to-[#000000] top-80% ">
+        <div className="flex justify-center bg-gradient-to-r from-sky-600 to-red-600 bg-clip-text text-transparent font-Inter font-medium  opacity-80 text-4xl">
+          But Don't Take Our Word For It
+        </div>
+        <div className="flex justify-center pt-4 text-white font-Inter text-7xl font-semibold">
+          Customer Testimonials
+        </div>
+
+
+      </div>
+
+      <div className="mx-auto py-12 ">
+        <ControlledCarousel></ControlledCarousel>
+      </div>
+
+
+      
     </div>
+  );
 }
