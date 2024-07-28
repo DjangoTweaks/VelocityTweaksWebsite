a4199cb87f94150dac24101dd9cb8e757b49b44e
Aadity Dawkar <dawkarad2002@gmail.com>
2024-07-29 00:45:19 +0530

    added auth

--- server/index.js
+++ server/index.js
@@ -2,19 +2,49 @@ const express = require("express");
 const path= require("path")
 const app =express();
 const cors = require('cors');
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const {restrictToLoggedInUserOnly} = require("./middlewares/auth");
+const passport = require("./config/passport");
 
+const userRoute = require("./routes/user")
+const staticRouter= require("./routes/staticRouter");
+const openRouter = require("./routes/openRouter");
+const authRouter = require("./routes/auth");
 
 const queryRoutes = require("./routes/queryRoutes");
 const reviewRoutes = require("./routes/reviewRoutes")
 
+const router= express.Router();
 
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+app.use(cookieParser());
 
+
+app.use(
+    session({
+      secret: "Aaditya@3737",
+      resave: false,
+      saveUninitialized: false,
+    })
+  );
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use("/user", userRoute);
+app.use("/home", restrictToLoggedInUserOnly, staticRouter);
+app.use("/open", openRouter);
+app.use("/auth", authRouter);
 
 app.use('/api/queries', queryRoutes);
 app.use('/api/review', reviewRoutes);
+
+app.set("view engine","ejs");
+app.set("views", path.resolve("./views"));
 
 const{connectMongoDB}= require('./connect')
 connectMongoDB('mongodb+srv://Aaditya:admin@cluster0.kxn151h.mongodb.net/D1')
