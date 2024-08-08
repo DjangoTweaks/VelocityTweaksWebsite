ff90bfa889cc2783cc32021e22fb0878de7e6d3d
Aadity Dawkar <dawkarad2002@gmail.com>
2024-08-08 18:47:30 +0530

    added stars in reviews

--- server/model/review.js
+++ server/model/review.js
@@ -1,29 +1,34 @@
 const mongoose = require('mongoose');
-const { type } = require('os');
-
 
 const reviewSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-    },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  product: {
+    type: String,
+    required: true,
+    enum: ['basic', 'premium'],
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 5,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-    email:{
-        type:String,
-        required: true,
-    },
-
-    message:{
-        type: String,
-        required: true,
-    },
-    
-    date:{
-        type: Date,
-        default: Date.now,
-    }
-
-})
-
-const REVIEW = mongoose.model("review", reviewSchema)
-module.exports= REVIEW
+const REVIEW = mongoose.model("review", reviewSchema);
+module.exports = REVIEW;
