f0673c01391ebdd4e7ca2b27d786703a320a7dcc
DjangoTweaks <djangotweaks@gmail.com>
2024-10-02 17:39:46 +0530

    added dashboard for keys and orders

--- client/Velocity Tweaks (Client Side)/src/pages/User/Dashboard/UserOrderView.jsx
+++ client/Velocity Tweaks (Client Side)/src/pages/User/Dashboard/UserOrderView.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MdOutlineShoppingBag } from "react-icons/md";
 import OrdersMap from "./OrdersMap";
+import { Link } from "react-router-dom";
 
 export default function UserOrderView() {
   const orders = [
@@ -24,27 +25,50 @@ export default function UserOrderView() {
         </div>
       </div>
       <div className="pl-4 pt-4 font-Inter font-semibold text-xl">Orders</div>
-
       <div className="bg-[#292929] rounded-md drop-shadow-2xl bg-opacity-50 mx-4  mt-4 p-3">
-        <table className="w-full">
-          <thead>
-            <tr className="font-Inter text-md">
-              <th className="font-normal text-start">Order ID</th>
-              <th className="font-normal">Payment Status</th>
-              <th className="font-normal text-end">Order Total</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.map((orders) => (
-              <OrdersMap
-                key={orders.orderID}
-                orderID={orders.orderID}
-                paymentStatus={orders.paymentStatus}
-                orderTotal={orders.orderTotal}
-              />
-            ))}
-          </tbody>
-        </table>
+        {orders.length !== 0 ? (
+          <div>
+            {" "}
+            <table className="w-full">
+              <thead className="border-b-[1px]  border-gray-400 h-[30px] border-opacity-50">
+                <tr className="font-Inter text-lg">
+                  <th className="font-normal text-start">Order ID</th>
+                  <th className="font-normal">Payment Status</th>
+                  <th className="font-normal text-end">Order Total</th>
+                </tr>
+              </thead>
+              <tbody>
+                {orders.map((orders) => (
+                  <OrdersMap
+                    key={orders.orderID}
+                    orderID={orders.orderID}
+                    paymentStatus={orders.paymentStatus}
+                    orderTotal={orders.orderTotal}
+                  />
+                ))}
+              </tbody>
+            </table>
+          </div>
+        ) : (
+          <div>
+            <div>
+              <div className="w-full py-5">
+                <h1 className="font-Inter font-semibold text-2xl text-center pt-3">
+                  No orders found. Ready to explore?
+                </h1>
+                <div className="flex justify-center mt-8 ">
+                  {" "}
+                  <Link to="/store">
+                    {" "}
+                    <button className="p-3 w-48 text-xl bg-white hover:bg-gray-300 rounded-lg hover:duration-150 hover:transition hover:ease-in-out text-black font-Inter font-bold ">
+                      Shop Now{" "}
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
