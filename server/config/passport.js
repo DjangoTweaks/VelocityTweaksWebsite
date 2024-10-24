dc979282549d0f76134e462e868cad9c97dc1bd5
Aadity Dawkar <dawkarad2002@gmail.com>
2024-10-25 04:55:32 +0530

    auth url

--- server/config/passport.js
+++ server/config/passport.js
@@ -11,7 +11,7 @@ passport.use('google-login',
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/auth/google/callback/login"
+      callbackURL: "https://velocity-tweaks-website-tau.vercel.app/auth/google/callback/login"
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
